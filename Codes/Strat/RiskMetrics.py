7fb5c767470e87d912f3d5f7a93d1198d09ab326
tailinks <ced29lam@gmail.com>
2023-11-04 16:00:33 -0400

    Merged data available

--- Codes/Strat/RiskMetrics.py
+++ Codes/Strat/RiskMetrics.py
@@ -1,3 +1,11 @@
+import pandas as pd
+
 class RiskMetrics:
-    def __init__(self) -> None:
+    def __init__(self,benchmark_directory, returns_directory) -> None:
+        self.benchmark = pd.read_csv(benchmark_directory)
+        self.benchmark['benchmark_return'] = self.benchmark['benchmark_return'] - 1
+        self.returns = pd.read_csv(returns_directory)
         
+if __name__ == "__main__":
+    test = RiskMetrics("Data/benchmark/SPY_returns.csv")
+    print(test.benchmark)
