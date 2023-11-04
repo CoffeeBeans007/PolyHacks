5380241077574baff3ec3b2f8123f00dccc81a25
Thomas Vaudescal <vaudescal.t@gmail.com>
2023-11-04 16:42:06 -0400

    [ADD] - file_management.py

---
+++ Codes/Strat/file_management.py
@@ -0,0 +1,13 @@
+import os
+import shutil
+import pandas as pd
+from typing import Union, Set, Any
+import importlib.util
+
+
+class FileManagement(object):
+    pass
+
+
+if __name__ == '__main__':
+    pass
