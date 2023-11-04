d19bbf269635bad3140e2b853a8293d6b452fc92
tailinks <ced29lam@gmail.com>
2023-11-04 11:10:56 -0400

    Made the merged data

--- fetcher.py
+++ fetcher.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-class data_input():
+class data_input:
     def __init__(self, path):
         self.path = path
         self.data = pd.read_csv(path)
@@ -16,8 +16,20 @@ class data_input():
             cleaned_columns.append(column.split('_')[-1])
         cleaned_columns.pop(0)
         return cleaned_columns
-
     
+    def get_data(self,):
+        return self.data
+
+class merged_data():
+    def __init__(self) -> None:
+        self.data_names = ['adjusted_close', 'close', 'high', 'low', 'open', 'volume', 'dividend', 'split']
+        self.data = pd.DataFrame()
+    def fetch_and_merge(self,):
+        for data_name in self.data_names:
+            data = data_input('Data/'+data_name+'.csv').get_data()
+            self.data = pd.concat([self.data, data], axis=1)
+            
 if __name__ == "__main__":
-    data = data_input('Data/low.csv')
-    print(data.get_all_tickers())
+    data = merged_data()
+    data.fetch_and_merge()
+    data.data.to_csv('Data/merged_data.csv')
